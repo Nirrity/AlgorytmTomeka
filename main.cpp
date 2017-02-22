1463360ce1da42f6b3e7db341db1305a2f55ad3d
slaby <slaby.konrad@gmail.com>
2017-02-23 00:50:02 +0100

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,35 +1,36 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 
 bool wczytajDaneZPliku(std::vector<std::vector<double>>& tempDane, 
-	int& kolumny, int& wiersze, std::string nazwaPliku);
-
+		std::string nazwaPliku);
 void wyswietlDane(std::vector<std::vector<double>>& tempDane);
+std::vector<std::vector<double>> algorytmTomeka(
+		std::vector<std::vector<double>>& tmpDane);
+double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
+	std::vector<double>& drugiPunkt);
 
 
 int main()
 {
-	
 	std::vector<std::vector<double>> dane_wejsciowe;
-	int liczbaKolumn=0, liczbaWierszy=0;
 
-	if (wczytajDaneZPliku(dane_wejsciowe,liczbaKolumn,liczbaWierszy,"Iris.txt"))
+	if (wczytajDaneZPliku(dane_wejsciowe,"Iris.txt"))
 	{
 		std::cout<<"Plik nie istnieje!"<<std::endl;
 	}
 
-	wyswietlDane(dane_wejsciowe);
-	
+	//wyswietlDane(dane_wejsciowe);
 
 	return 0;
 }
 
 
 bool wczytajDaneZPliku(std::vector<std::vector<double>>& tempDane, 
-	int& kolumny, int& wiersze, std::string nazwaPliku){
-
+		std::string nazwaPliku)
+{
 	std::ifstream input;
 	input.open(nazwaPliku);
 
@@ -38,16 +39,16 @@ bool wczytajDaneZPliku(std::vector<std::vector<double>>& tempDane,
 		return true;
 	}
 
-	int liczbaKlas=0;
-	input>>liczbaKlas>>kolumny>>wiersze;
-	++kolumny;
+	int liczbaKlas=0, liczbaKolumn=0, liczbaWierszy=0;
+	input>>liczbaKlas>>liczbaKolumn>>liczbaWierszy;
+	++liczbaKolumn;
 
-	for (int i = 0; i < wiersze; ++i)
+	for (int i = 0; i < liczbaWierszy; ++i)
 	{
-		std::vector<double> t;
-		t.resize(kolumny);
-		tempDane.push_back(t);
-		for (int j = 0; j < kolumny; ++j)
+		std::vector<double> tmpVector;
+		tmpVector.resize(liczbaKolumn);
+		tempDane.push_back(tmpVector);
+		for (int j = 0; j < liczbaKolumn; ++j)
 		{
 			input>>tempDane[i][j];
 		}
@@ -58,11 +59,47 @@ bool wczytajDaneZPliku(std::vector<std::vector<double>>& tempDane,
 }
 
 
-void wyswietlDane(std::vector<std::vector<double>>& tempDane){
+void wyswietlDane(std::vector<std::vector<double>>& tempDane)
+{
 	for(auto& wiersze: tempDane){
 		for (auto& kolumny : wiersze)
 		{
 			std::cout<<kolumny<<"  ";
 		}std::cout<<std::endl;
 	}
+}
+
+
+double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
+	std::vector<double>& drugiPunkt)
+{
+	int rozmiar = pierwszyPunkt.size();
+	double wynik = 0.0;
+
+	for (int i = 0; i < rozmiar; ++i)
+	{
+		wynik += std::pow(pierwszyPunkt[i]-drugiPunkt[i], 2.0);
+	}
+
+	return wynik;
+}
+
+
+std::vector<std::vector<double>> algorytmTomeka(
+	std::vector<std::vector<double>>& tmpDane)
+{
+	int liczbaWierszy = tmpDane.size();
+	int liczbaKolumn = tmpDane[0].size();
+
+	double odleglosc = 0.0;
+
+	std::vector<std::vector<double>> wynik;
+	wynik.push_back(tmpDane[0]);
+
+	for (int i = 0; i < liczbaWierszy; ++i)
+	{
+		/* code */
+	}
+
+	return wynik;
 }
