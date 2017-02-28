3a68099b44de7e9aed50295e41d8bd3799c6c410
slabykonrad <konrad.slaby@wp.pl>
2017-02-28 21:19:28 +0100

    wersja beta algorytmu Harta

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <random>
 
 
 bool wczytajDaneZPliku(std::vector<std::vector<double>>& tempDane, 
 		std::string nazwaPliku);
 void wyswietlDane(std::vector<std::vector<double>>& tempDane);
-std::vector<std::vector<double>> algorytmTomeka(
-		std::vector<std::vector<double>>& tmpDane);
+std::vector<std::vector<double>> algorytmHarta(
+		std::vector<std::vector<double>> tmpDane);
 double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
 	std::vector<double>& drugiPunkt);
 
@@ -16,13 +17,18 @@ double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
 int main()
 {
 	std::vector<std::vector<double>> dane_wejsciowe;
+	std::vector<std::vector<double>> dane_wyjsciowe;
 
-	if (wczytajDaneZPliku(dane_wejsciowe,"Iris.txt"))
+	if (wczytajDaneZPliku(dane_wejsciowe,"Epj.txt"))
 	{
 		std::cout<<"Plik nie istnieje!"<<std::endl;
 	}
 
 	//wyswietlDane(dane_wejsciowe);
+	dane_wyjsciowe = algorytmHarta(dane_wejsciowe);
+	wyswietlDane(dane_wyjsciowe);
+	std::cout<<"\n";
+	std::cout<<"Liczba znalezionych probek: "<<dane_wyjsciowe.size()<<std::endl;
 
 	return 0;
 }
@@ -76,7 +82,7 @@ double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
 	int rozmiar = pierwszyPunkt.size();
 	double wynik = 0.0;
 
-	for (int i = 0; i < rozmiar; ++i)
+	for (int i = 1; i < rozmiar; ++i)
 	{
 		wynik += std::pow(pierwszyPunkt[i]-drugiPunkt[i], 2.0);
 	}
@@ -85,20 +91,45 @@ double odlegloscPomiedzyDwomaPunktami(std::vector<double>& pierwszyPunkt,
 }
 
 
-std::vector<std::vector<double>> algorytmTomeka(
-	std::vector<std::vector<double>>& tmpDane)
+std::vector<std::vector<double>> algorytmHarta(
+	std::vector<std::vector<double>> tmpDane)
 {
-	int liczbaWierszy = tmpDane.size();
-	int liczbaKolumn = tmpDane[0].size();
-
-	double odleglosc = 0.0;
-
 	std::vector<std::vector<double>> wynik;
-	wynik.push_back(tmpDane[0]);
+	int maxIloscProbek = tmpDane.size();
+	int iloscWylosowanychProbek = 0;
+	std::random_device rd;
+    std::mt19937 gen(rd());
 
-	for (int i = 0; i < liczbaWierszy; ++i)
-	{
-		/* code */
+	while(iloscWylosowanychProbek <= maxIloscProbek) {
+		++iloscWylosowanychProbek;
+		int obecnyRozmiar = tmpDane.size();
+		std::uniform_int_distribution<> dist(0, obecnyRozmiar-1);
+		int wylosowanaProbka = dist(gen);
+		double aktualnaNajblizszaOdleglosc = 0.0;
+		int najblizszyIndex = 0;
+
+		for (int i = 0; i < obecnyRozmiar; ++i)
+		{
+			if(i == wylosowanaProbka)
+				continue;
+
+			double liczeNajblizszaOdleglosc = 
+					odlegloscPomiedzyDwomaPunktami(tmpDane[wylosowanaProbka],tmpDane[i]);
+		
+			if(aktualnaNajblizszaOdleglosc == 0.0){
+				aktualnaNajblizszaOdleglosc = liczeNajblizszaOdleglosc;
+				najblizszyIndex = i;
+			}
+			else if(liczeNajblizszaOdleglosc < aktualnaNajblizszaOdleglosc){
+				aktualnaNajblizszaOdleglosc = liczeNajblizszaOdleglosc;
+				najblizszyIndex = i;
+			}
+		}
+
+		if(tmpDane[wylosowanaProbka][0] != tmpDane[najblizszyIndex][0]){
+			wynik.push_back(tmpDane[najblizszyIndex]);
+			tmpDane.erase(tmpDane.begin()+najblizszyIndex);
+		}
 	}
 
 	return wynik;
